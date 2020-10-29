079e41034b1738434150e28c43a956ce6ab7fc68
Luka Vuk <57720243+lvuk@users.noreply.github.com>
2020-10-29 22:39:26 +0100

    Done.

---
+++ DZ1/Program/Episode/Episode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Episode1
+{
+    public class Episode
+    {
+        private int viewerCount;
+        private double gradeSum;
+        private double maxGrade;
+       
+        public Episode(int viewerCount, double gradeSum, double maxGrade)
+        {
+            this.viewerCount = viewerCount;
+            this.gradeSum = gradeSum;
+            this.maxGrade = maxGrade;
+
+        }
+
+        public Episode()
+        {
+        }
+
+        public void AddView(double randNum)
+        {
+            viewerCount++;
+            gradeSum += randNum;
+            if (randNum > maxGrade)
+                maxGrade = randNum;
+
+        }
+
+        public double GetViewerCount()
+        {
+            return viewerCount;
+        }
+
+        public double GetAverageScore()
+        {
+            return gradeSum / viewerCount;
+        }
+
+        public double GetMaxScore()
+        {
+            return maxGrade;
+        }
+    }
+}
---
+++ DZ1/Program/Program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Episode1;
+
+namespace Program
+{
+	class Program
+	{
+		private static readonly Random _random = new Random();
+
+		public static double GenerateRandomScore()
+		{
+			return _random.Next(0, 10);
+		}
+
+		static void Main(string[] args)
+		{
+			Episode ep1, ep2;
+			ep1 = new Episode();
+			ep2 = new Episode(10, 64.39, 8.7);
+			int viewers = 10;
+			for (int i = 0; i < viewers; i++)
+			{
+				ep1.AddView(GenerateRandomScore());
+				Console.WriteLine(ep1.GetMaxScore());
+			}
+			if (ep1.GetAverageScore() > ep2.GetAverageScore())
+			{
+				Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+			}
+			else
+			{
+				Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+			}
+		}
+	}
+}
